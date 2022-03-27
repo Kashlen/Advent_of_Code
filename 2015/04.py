6a76714b5a6bd02572d8dac190b031fa598ec9f1
Lenka Kášová <kasova.lenka@email.cz>
2022-03-27 18:18:40 +0200

    1st part of 4th day added

---
+++ 2015/04.py
@@ -0,0 +1,21 @@
+from cgitb import text
+import hashlib
+
+# 1st part
+
+s_key = "bgvyzdsv"
+
+def hash_number(key):
+    key_in_MD5 = hashlib.md5(key.encode())
+    hexadecimal_hash = key_in_MD5.hexdigest()
+    return hexadecimal_hash
+
+def find_5_zeros(key):
+    for x in range(10000000):
+        secret_key = key + str(x)
+        if '00000'in hash_number(secret_key)[0:5]:
+            print(x)
+            quit()
+
+
+find_5_zeros(s_key)
